2bd5951d0360d63ee0bc5cd1be93499c5e31b5a3
Thaíse <alves.thaisesilva@gmail.com>
2026-09-23 04:01:29 -0300

    Refactor Clube class to initialize obra and encontro using IFabricaAbstrata

--- src/main/java/abstractfactory/app/Clube.java
+++ src/main/java/abstractfactory/app/Clube.java
@@ -1,5 +1,20 @@
 package abstractfactory.app;
 
 public class Clube {
-    
+
+    private IObra obra;
+    private IEncontro encontro;
+
+    public Clube(IFabricaAbstrata fabrica) {
+        this.obra = fabrica.criarObra();
+        this.encontro = fabrica.criarEncontro();
+    }
+
+    public String escolherObra() {
+        return this.obra.escolherObra();
+    }
+
+    public String escolherEncontro() {
+        return this.encontro.escolherEncontro();
+    }
 }
